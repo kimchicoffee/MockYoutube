f4525b3378822e8b680a5cd2055f202178e9058c
Sujin Lee <pasdesujin@gmail.com>
2016-06-24 19:57:22 -0400

    pass video player mocha test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer video={window.exampleVideoData[0]} />
     </div>
     <div className="col-md-5">
       <VideoList videos={window.exampleVideoData} />
