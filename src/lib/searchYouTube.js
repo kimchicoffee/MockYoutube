27dfb747211838782be1ff549a07fbe84ac89d76
Sujin Lee <pasdesujin@gmail.com>
2016-06-25 15:39:19 -0400

    add youtube api

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,42 @@
+// var searchYouTube = (options, callback) => {
+//   // TODO
+//   $.ajax({
+//     url: 'https://www.googleapis.com/youtube/v3/search',
+//     type: 'GET',
+//     data: options,
+//     success: function(data){
+//       console.log(data);
+//       // callback();
+//     },
+//     error: function(error){
+//       console.error('something is wrong with the data', error)
+//     }
+//
+//   })
+// };
+
 var searchYouTube = (options, callback) => {
-  // TODO
+  console.log(window.YOUTUBE_API_KEY);
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    key: window.YOUTUBE_API_KEY,
+    q: 'kittens',
+    maxResults: 5,
+    part: 'snippet',
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+  .done(({items}) => {
+    if (callback) {
+      callback(items);
+    }
+  })
+  .fail((error) => {
+    console.error(error);
+  })
 };
+
+searchYouTube();
+
+
 
 window.searchYouTube = searchYouTube;
