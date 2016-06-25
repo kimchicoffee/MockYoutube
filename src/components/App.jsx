569fb4428bdecbc38446bb373a1aebc3a63979b8
Sujin Lee <pasdesujin@gmail.com>
2016-06-24 23:00:25 -0400

    app test passes except click event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer video={window.exampleVideoData[0]} />
-    </div>
-    <div className="col-md-5">
-      <VideoList videos={window.exampleVideoData} />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={window.exampleVideoData[0]} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={window.exampleVideoData} />
+        </div>
+      </div>
+    )
+  }
+
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
